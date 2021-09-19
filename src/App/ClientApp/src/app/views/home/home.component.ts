8ea503a1406fe8a1f86d49cd301475cb694ed7ad
derek-baker <dbaker@sdgnys.com>
2021-09-19 16:32:10 -0400

    RP524 initial submission: display submission ID to user after submisssion as fallback

--- src/App/ClientApp/src/app/views/home/home.component.ts
+++ src/App/ClientApp/src/app/views/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
-    //
+    public SubmissionId: string;
+
+    constructor(private readonly router: Router) {
+        this.SubmissionId = this.router.getCurrentNavigation()?.extras?.state?.grievanceId;
+    }
 }
